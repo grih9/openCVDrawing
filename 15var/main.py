47e038e63890f0c89386bb29b0694fcc1fc483c4
Tolstikov Grigoriy <tolstikov.gn@edu.spbstu.ru>
2020-05-09 16:30:05 +0300

    некоторые изменения и улучшения, +- работающая версия

--- 15var/main.py
+++ 15var/main.py
@@ -25,17 +25,17 @@ upper_yellow = np.array([40, 255, 255], dtype = "uint8")
 lower_orange = np.array([12, 85, 110], dtype = "uint8")
 upper_orange = np.array([23, 255, 255], dtype = "uint8")
 
-lower_violet = np.array([135, 85, 110], dtype = "uint8")
-upper_violet = np.array([150, 255, 255], dtype = "uint8")
+lower_violet = np.array([130, 50, 50], dtype = "uint8")
+upper_violet = np.array([160, 255, 255], dtype = "uint8")
 
 lower_black = np.array([0, 0, 0], dtype = "uint8")
 upper_black = np.array([0, 255, 255], dtype = "uint8")
 
-lower_blue = np.array([100, 85, 110], dtype = "uint8")
-upper_blue = np.array([115, 255, 255], dtype = "uint8")
+lower_blue = np.array([98, 109, 20], dtype = "uint8")
+upper_blue = np.array([112, 255, 255], dtype = "uint8")
 
-lower_green = np.array([30, 85, 110], dtype = "uint8")
-upper_green = np.array([80, 255, 255], dtype = "uint8")
+lower_green = np.array([0, 50, 50], dtype = "uint8")
+upper_green = np.array([180, 255, 255], dtype = "uint8")
 
 color = ""
 x = 0.0
@@ -48,16 +48,13 @@ pts = deque(maxlen=args["buffer"])
 while videoCapture.isOpened():
     ret, frame = videoCapture.read()
     ret2, me = videoMe.read()
-    frame = cv.flip(frame, 1)
+    #frame = cv.flip(frame, 1)
 
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     red_tmp = cv.inRange(hsv, lower_red, upper_red)
     violet_red = cv.inRange(hsv, lower_violet_red, upper_violet_red)
     yellow = cv.inRange(hsv, lower_yellow, upper_yellow)
-    orange = cv.inRange(hsv, lower_orange, upper_orange)
-    violet = cv.inRange(hsv, lower_violet, upper_violet)
     blue = cv.inRange(hsv, lower_blue, upper_blue)
-    black = cv.inRange(hsv, lower_black, upper_black)
     green = cv.inRange(hsv, lower_green, upper_green)
     red = red_tmp + violet_red
 
@@ -73,7 +70,7 @@ while videoCapture.isOpened():
         y_moment = moments['m01']
         tmpArea = moments['m00']
         if color == "yellow":
-            if tmpArea != 0:
+            if tmpArea > 1000:
                 x = x_moment
                 y = y_moment
                 area = tmpArea
@@ -83,20 +80,19 @@ while videoCapture.isOpened():
                 color = ""
                 mask = None
                 area = 0
-        elif tmpArea != 0 and tmpArea > maxArea:
+        elif tmpArea > 1000 and tmpArea > maxArea:
             tmpMask = yellow
             maxArea = tmpArea
             tmpColor = "yellow"
             tmpx = x_moment
             tmpy = y_moment
 
-    for k in range(1):
         moments = cv.moments(red, 1)
         x_moment = moments['m10']
         y_moment = moments['m01']
         tmpArea = moments['m00']
         if color == "red":
-            if tmpArea != 0:
+            if tmpArea > 1000:
                 x = x_moment
                 y = y_moment
                 area = tmpArea
@@ -106,13 +102,34 @@ while videoCapture.isOpened():
                 color = ""
                 mask = None
                 area = 0
-        elif tmpArea != 0 and tmpArea > maxArea:
+        elif tmpArea > 1000 and tmpArea > maxArea:
             tmpMask = red
             maxArea = tmpArea
             tmpColor = "red"
             tmpx = x_moment
             tmpy = y_moment
 
+        moments = cv.moments(blue, 1)
+        x_moment = moments['m10']
+        y_moment = moments['m01']
+        tmpArea = moments['m00']
+        if color == "blue":
+            if tmpArea > 1000:
+                x = x_moment
+                y = y_moment
+                area = tmpArea
+                mask = blue
+                break
+            else:
+                color = ""
+                mask = None
+                area = 0
+        elif tmpArea > 1000 and tmpArea > maxArea:
+            tmpMask = blue
+            maxArea = tmpArea
+            tmpColor = "blue"
+            tmpx = x_moment
+            tmpy = y_moment
 
     if color == "":
         color = tmpColor
@@ -121,37 +138,50 @@ while videoCapture.isOpened():
         x = tmpx
         y = tmpy
 
-    if (area > 100) :
+    if (area > 1000) :
         x = int(x / area)
         y = int(y / area)
-        cv.putText(frame, color, (x, y), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        if isRealesed:
+            cv.putText(frame, color, (x, y), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+        else:
+            cv.putText(frame, color, (x, y), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
         centre = (x, y)
         cnts = cv.findContours(mask, cv.RETR_EXTERNAL,
                                 cv.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
         c = max(cnts, key=cv.contourArea)
-        extBot = tuple(c[c[:, :, 1].argmax()][0])
-        print("Centre : " + str(centre) + ", farthest Point : " + str(extBot))
-        cv.circle(frame, extBot, 5, [255, 0, 255], -1)
-        lst = list(extBot)
-        lst[1] = 450 - lst[1]
-        extBot = tuple(lst)
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        print("Centre : " + str(centre) + ", top point : " + str(extTop))
+        cv.circle(frame, extTop, 5, [255, 0, 255], -1)
+
         if isRealesed:
-            pts.appendleft(extBot)
+            pts.appendleft(extTop)
 
             for i in range(1, len(pts)):
+                d = list(extTop)
+                tmp = list(pts[0])
+                if ((tmp[0] - d[0])**2 + (tmp[1] - d[1])**2)**(1/2) > 50:
+                    pts.popleft()
+                    continue
                 if pts[i - 1] is None or pts[i] is None:
                     continue
 
                 colorDraw = (0, 0, 255)
                 if color == "yellow":
                     colorDraw = (0, 255, 255)
+                elif color == "blue":
+                    colorDraw = (255, 0, 0)
                 cv.line(planeList, pts[i - 1], pts[i], colorDraw)
 
+    blueScreen = cv.bitwise_and(frame, frame, mask = blue)
+    redScreen = cv.bitwise_and(frame, frame, mask = red)
+    yellowScreen = cv.bitwise_and(frame, frame, mask = yellow)
     cv.imshow("frame", frame)
-    cv.imshow('yellowmask', yellow)
-    cv.imshow('redmask', red)
+    cv.imshow('yellowmask', yellowScreen)
+    cv.imshow('redmask', redScreen)
     cv.imshow('draw', planeList)
+    cv.imshow('blue', blueScreen)
+    #cv.imshow('me', me)
 
     pressedKey = cv.waitKey(1)
     if pressedKey & 0xFF == ord('z'):
